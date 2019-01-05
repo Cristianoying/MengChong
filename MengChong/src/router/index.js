5ec6ae3044f925dede2462166d228c31112c8710
Cristianoying <3050504934@qq.com>
2019-01-05 22:11:49 +0800

    王营1页面全局搭建

--- MengChong/src/router/index.js
+++ MengChong/src/router/index.js
@@ -1,15 +1,55 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Home from '@/components/home/home'
+import Community from "@/components/community/community"
+import Petlist from "@/components/petlist/petlist"
+import Mine from "@/components/mine/mine"
+import Login from "@/components/login/login"
+import Shopcart from "@/components/shopcart/shopcart"
+import Publish from "@/components/publish/publish"
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect:'/home'
+    },
+    {
+    	path:'/home',
+    	name:'home',
+      	component: Home
+    },
+    {
+    	path:'/community',
+    	name:'community',
+    	component:Community
+    },
+    {
+    	path:'/petlist',
+    	name:'petlist',
+    	component:Petlist
+    },
+    {
+    	path:'/mine',
+    	name:'mine',
+    	component:Mine
+    },
+    {
+    	path:'/login',
+    	name:"login",
+    	component:Login
+    },
+    {
+    	path:'/shopcart',
+    	name:'shopcart',
+    	component:Shopcart
+    },
+    {
+    	path:'/publish',
+    	name:'publish',
+    	component:Publish
+
     }
   ]
 })
