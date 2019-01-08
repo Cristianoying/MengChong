793bdf6d772fe96e3d83d76ab437519e953b81aa
yuxin12 <46389504+yuxin12@users.noreply.github.com>
2019-01-08 13:39:11 +0800

    Update main.js

--- MengChong/src/main.js
+++ MengChong/src/main.js
@@ -15,13 +15,9 @@ import MintUi from 'mint-ui'
 import 'mint-ui/lib/style.css'
 Vue.use(MintUi);
 
-import './common/css/footer_icon_d/iconfont.css'
-import MintUi from 'mint-ui';
-import 'mint-ui/lib/style.min.css'
 import axios from 'axios'
 
 Vue.config.productionTip = false
-Vue.use( MintUi );
 
 Vue.prototype.$axios=axios
 import store from './store'
