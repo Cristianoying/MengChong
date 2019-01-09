6cf5c5f444655aa15709998c27cc86827be2c797
Cristianoying <39425195+Cristianoying@users.noreply.github.com>
2019-01-09 08:31:24 +0800

    Update mutations.js

--- MengChong/src/store/selectedata/mutations.js
+++ MengChong/src/store/selectedata/mutations.js
@@ -1,12 +1,5 @@
 export default{
     handleSelect(state,params){
         state.selectList=params;
-
-        // console.log(state);
-<<<<<<< HEAD:MengChong/src/store/selected/mutations.js
-=======
-        // console.log(state.selectList)
->>>>>>> yangbo:MengChong/src/store/selectedata/mutations.js
-        
     }
 }
